7c73f75f40aa00c4f74f820446879a481f697b02
Qbot <kubacembrzynski97@o2.pl>
2021-01-20 18:53:00 +0100

    Added DoctorServiceTest

--- src/test/java/com/cembrzynski/clinic/service/DoctorServiceTest.java
+++ src/test/java/com/cembrzynski/clinic/service/DoctorServiceTest.java
@@ -1,7 +1,35 @@
 package com.cembrzynski.clinic.service;
 
+import com.cembrzynski.clinic.data.repository.DoctorRepository;
+import com.cembrzynski.clinic.error.exception.EntityNotFoundException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 class DoctorServiceTest {
-    //TODO add tests
+
+    @Mock
+    private DoctorRepository doctorRepository;
+    private DoctorService doctorService;
+
+    @BeforeEach
+    public void initService(){
+        MockitoAnnotations.openMocks(this);
+        doctorService = new DoctorService();
+        doctorService.setDoctorRepository(doctorRepository);
+    }
+
+    @Test
+    public void shouldQueryForDoctor(){
+        Long id = 1L;
+
+        assertThrows(EntityNotFoundException.class, () -> doctorService.findDoctorById(id));
+
+        verify(doctorRepository, times(1)).findById(id);
+    }
+
 }
